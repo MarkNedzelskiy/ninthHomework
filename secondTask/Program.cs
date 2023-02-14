7fd14fbfda680ef6f98862a75c0d0f977b7a3852
Mark <nedzelsky.mark@mail.ru>
2023-02-14 20:45:44 +0300

    second task done

---
+++ secondTask/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int CountNumbersRecursion( int m, int n)
+{
+    int summ = 0;
+    if (m <= n)
+    {
+        summ += m + CountNumbersRecursion(m + 1, n);
+    }
+    return summ;
+}
+
+int numberM = GetNumber("Введите число M:");
+int numberN = GetNumber("Введите число N:");
+int summ = 0;
+summ = CountNumbersRecursion(numberM, numberN);
+Console.WriteLine(summ);
